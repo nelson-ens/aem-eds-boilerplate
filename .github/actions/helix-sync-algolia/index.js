537510bbbbc81b977d7af642d00dbd5a6da946a8
Nelson Wei <nelson@ensemble.com>
2025-01-20 10:25:07 -0800

    test

---
+++ .github/actions/helix-sync-algolia/index.js
@@ -0,0 +1,10 @@
+﻿import core from '@actions/core';
+import github from '@actions/github';
+
+async function run() {
+  console.log('Logging github event payload: ', JSON.stringify(github));
+}
+
+run().catch((error) => {
+  core.setFailed(`Action failed with error: ${error.message}`);
+});
