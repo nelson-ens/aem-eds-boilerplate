41b7e2d3789b3ab29c9cd74bfee9f42c7294a311
Nelson Wei <nelson@ensemble.com>
2025-01-20 16:50:35 -0800

    Test

--- .github/actions/helix-algolia-sync-ts/src/index.ts
+++ .github/actions/helix-algolia-sync-ts/src/index.ts
@@ -1,11 +1,9 @@
-import { getInput, setFailed } from "@actions/core";
-import { context, getOctokit } from "@actions/github";
-import { faker } from '@faker-js/faker';
-import { algoliasearch } from "algoliasearch";
-import { createHash } from 'crypto';
-import { fetchHelixResourceMetadata } from "./utils/edsUtils";
-import { md5 } from "./utils/stringUtils";
-
+import {getInput, setFailed} from '@actions/core';
+import {context} from '@actions/github';
+import {faker} from '@faker-js/faker';
+import {algoliasearch} from 'algoliasearch';
+import {fetchHelixResourceMetadata} from './utils/edsUtils';
+import {md5} from './utils/stringUtils';
 
 const run = async () => {
   console.log('Logging github event context: ', JSON.stringify(context));
@@ -34,42 +32,47 @@ const run = async () => {
   const eventType = context.payload.action;
   console.log('Logging eventType: ', eventType);
   try {
+    const helixResourceMetadata = await fetchHelixResourceMetadata(
+      clientPayload.org,
+      clientPayload.site,
+      branchName,
+      clientPayload.path,
+    );
 
-  const helixResourceMetadata = await fetchHelixResourceMetadata(
-    clientPayload.org,
-    clientPayload.site,
-    branchName,
-    clientPayload.path
-  );
+    console.log(
+      'Logging helixResourceMetadata: ',
+      JSON.stringify(helixResourceMetadata),
+    );
 
-  console.log('Logging helixResourceMetadata: ', JSON.stringify(helixResourceMetadata));
+    const slug = faker.lorem.slug();
+    const resourcePath = `/blogs/${slug}.md`;
 
-  const slug = faker.lorem.slug();
-  const resourcePath = `/blogs/${slug}.md`;
+    const record = {
+      webPath: `/blogs/${slug}`,
+      resourcePath: `${resourcePath}`,
+      name: `${faker.food.dish()}`,
+      lastModified: `${faker.date.anytime().getTime()}`,
+      title: `${faker.food.dish()}`,
+      image: `${faker.image.url()}`,
+      description: `${faker.food.description()}`,
+      category: `${faker.food.ethnicCategory()}`,
+      author: `${faker.book.author()}`,
+      date: `${faker.date.anytime().getTime()}`,
+    };
+    console.log('Logging record: ', record);
 
-  const record = {
-    "webPath": `/blogs/${slug}`,
-    "resourcePath": `${resourcePath}`,
-    "name": `${faker.food.dish()}`,
-    "lastModified": `${faker.date.anytime().getTime()}`,
-    "title": `${faker.food.dish()}`,
-    "image": `${faker.image.url()}`,
-    "description": `${faker.food.description()}`,
-    "category": `${faker.food.ethnicCategory()}`,
-    "author": `${faker.book.author()}`,
-    "date": `${faker.date.anytime().getTime()}`
-  };
-  console.log('Logging record: ', record);
-
-  const algAddOrUpdateObjResponse = await client.addOrUpdateObject({
-    indexName: indexName,
-    objectID: md5(resourcePath),
-    body: record
-  });
-
+    const algAddOrUpdateObjResponse = await client.addOrUpdateObject({
+      indexName,
+      objectID: md5(resourcePath),
+      body: record,
+    });
   } catch (error) {
-    setFailed('Action failed with error: ' + ((error as Error)?.message ?? "Unknown error"));
+    setFailed((error as Error)?.message ?? 'Unknown error');
   }
 };
 
-run();
+run().catch(error => {
+  console.error(
+    `Action failed with error: ${(error as Error)?.message ?? 'Unknown error'}`,
+  );
+});
--- .github/actions/helix-algolia-sync-ts/src/utils/edsUtils.ts
+++ .github/actions/helix-algolia-sync-ts/src/utils/edsUtils.ts
@@ -6,14 +6,20 @@
  * @param path {string}
  * @returns {Promise<{webPath: string, resourcePath: string, results: {name: string, record: Record<string, any>}[]}>}
  */
-export const fetchHelixResourceMetadata = async (owner, repo, branch, path) => {
-  path = path.replace(/^\/*/, '');
-  const url = new URL(`https://admin.hlx.page/index/${owner}/${repo}/${branch}/${path}`);
+const fetchHelixResourceMetadata = async (owner, repo, branch, path) => {
+  const modPath = path.replace(/^\/*/, '');
+  const url = new URL(
+    `https://admin.hlx.page/index/${owner}/${repo}/${branch}/${modPath}`,
+  );
   console.log(`Fetching Helix resource metadata from ${url}`);
 
   const response = await fetch(url);
   if (!response.ok)
-    throw new Error(`Failed to fetch Helix resource metadata: ${response.status} ${response.statusText}`);
+    throw new Error(
+      `Failed to fetch Helix resource metadata: ${response.status} ${response.statusText}`,
+    );
 
-  return await response.json();
+  return response.json();
 };
+
+export default {fetchHelixResourceMetadata};
