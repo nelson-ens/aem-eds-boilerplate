65a54e32472048314a3842295a0f442f277ea348
Nelson Wei <nelson@ensemble.com>
2025-01-24 10:40:25 -0800

    Suppoort multiple hbspt forms

--- blocks/hubspot-embed/hubspot-embed.js
+++ blocks/hubspot-embed/hubspot-embed.js
@@ -1,31 +1,28 @@
+import { div } from '../../scripts/dom-helpers.js';
 import { loadScript } from '../../scripts/aem.js';
+import generateId from '../../scripts/stringHelper.js'
 
-const loadScript2 = (block, { jsUrl, portalId, formId }, type) => {
-  console.log('logging loadScript2', { jsUrl, portalId, formId });
-  const script = document.createElement('script');
-  if (type) {
-    script.setAttribute('type', type);
-  }
+const embedHubspot = async (block, {
+  jsUrl, portalId, formId, target,
+}) => {
+  console.log('logging embedHubspot', {
+    jsUrl, portalId, formId, target,
+  });
 
-  script.text = ''
-      + '    hbspt.forms.create({'
-      + `      portalId: '${portalId}',`
-      + `     formId: '${formId}',`
-      + '    });';
-
-  console.log('logging loadScript2', script);
-  block.append(script);
-  return script;
+  await loadScript(`${jsUrl}?t=${target}`);
+  hbspt.forms.create({
+    portalId,
+    formId,
+    target: `#${target}`,
+  });
 };
 
-const embedHubspot = async (block, { jsUrl, portalId, formId }) => {
-  console.log('logging embedHubspot', { jsUrl, portalId, formId });
-  await loadScript(jsUrl);
-  loadScript2(block, { jsUrl, portalId, formId }, 'text/javascript');
-};
-
-const loadEmbed = async (block, { jsUrl, portalId, formId }) => {
-  console.log('logging loadEmbed', { jsUrl, portalId, formId });
+const loadEmbed = async (block, {
+  jsUrl, portalId, formId, target,
+}) => {
+  console.log('logging loadEmbed', {
+    jsUrl, portalId, formId, target,
+  });
   if (block.classList.contains('embed-is-loaded')) {
     console.log('  contains "embed-is-loaded", exit');
     return;
@@ -40,7 +37,9 @@ const loadEmbed = async (block, { jsUrl, portalId, formId }) => {
 
   const config = EMBEDS_CONFIG.find((e) => e.match.some((match) => jsUrl.includes(match)));
   if (config) {
-    await config.embed(block, { jsUrl, portalId, formId });
+    await config.embed(block, {
+      jsUrl, portalId, formId, target,
+    });
     block.classList = `block embed embed-${config.match[0]}`;
   } else {
     block.classList = 'block embed';
@@ -48,19 +47,29 @@ const loadEmbed = async (block, { jsUrl, portalId, formId }) => {
   block.classList.add('embed-is-loaded');
 };
 
-export default async function decorate(block) {
+export default async function decorate(block, index) {
   console.debug('hubspot-embed', block);
   const props = block.querySelectorAll('p');
   const jsUrl = props[0].innerHTML;
   const portalId = props[1].innerHTML;
   const formId = props[2].innerHTML;
   console.debug('logging', { jsUrl, portalId, formId });
-  block.textContent = '';
+  const target = `hubspot-embed-${generateId(5)}`;
+  console.debug('target', { target });
+  const form = div({
+    id: target,
+    class: 'hubspot-form',
+  });
+
+  block.innerHTML = '';
+  block.appendChild(form);
 
   const observer = new IntersectionObserver((entries) => {
     if (entries.some((e) => e.isIntersecting)) {
       observer.disconnect();
-      loadEmbed(block, { jsUrl, portalId, formId });
+      loadEmbed(block, {
+        jsUrl, portalId, formId, target,
+      });
     }
   });
   observer.observe(block);
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,2 @@
 // add delayed functionality here
+console.log('loading delayed.js');
